d10374efed5cbbadee4eda2b305bb07aa6c3ff85
YE CAPITAL SOLUTIONS <naeemmohd614@gmail.com>
2025-12-06 16:30:32 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBKLiwOclYyjEkyaxaGnKIZnGXYlwprAdo",
   authDomain: "naeem-dadaa.firebaseapp.com",
   projectId: "naeem-dadaa",
-  storageBucket: "naeem-dadaa.firebasestorage.app",
+  storageBucket: "naeem-dadaa.appspot.com",
   messagingSenderId: "849246117474",
   appId: "1:849246117474:web:b973e3098bd019a4c83b3b",
   measurementId: "G-NQWSWB1B22"
